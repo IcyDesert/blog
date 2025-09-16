d033fe279e4ab99fdf34a2dfd190c071730cd3ce
IcyDesert <icy_desert@outlook.com>
2025-09-16 10:45:30 +0800

    frame

---
+++ src/data/quotes.ts
@@ -0,0 +1,14 @@
+const _quotes_: string[] = [
+    "温馨的光境不过借出到期拿回吗",
+    "过往的回归分析从不是未来的无偏估计",
+    "Fight or flight?",
+    "Cogito, ergo sum",
+    "《上海交通大学生存手册》！",
+    "Also try Minecraft!",
+    "Also try Minceraft!",
+    "Work and rave!",
+    "fuzz...buzz...zzz...",
+    "Also available on GitHub!"
+]
+
+export const _quotes = Buffer.from(JSON.stringify(_quotes_)).toString('base64');
