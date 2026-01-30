e94fa31c69a78036fef757b238d7adc54269335b
IcyDesert <icy_desert@outlook.com>
2026-01-30 10:01:59 +0800

    feat: add friends' descriptions

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -8,7 +8,7 @@ export interface Friend {
 export const insights: Friend[] = [
 	{
 		title: "上海交通大学生存手册",
-		description: "真正的大学生第一课。另可找到<strong>免费</strong> PDF 版本。",
+		description: "真正的大学生第一课",
 		url: "https://survivesjtu.gitbook.io/survivesjtumanual",
 	},
 	{
@@ -19,42 +19,48 @@ export const insights: Friend[] = [
 ];
 
 export const friends: Friend[] = [
-	// {
-	// 	title: "cclvi256",
-	// 	url: "https://cclvi.cc/",
-	// 	avatar: "/linkx/cclvi256.jpg",
-	// },
-	// {
-	// 	title: "VaalaCat",
-	// 	avatar: "/linkx/vaalacat.jpg",
-	// 	url: "https://vaala.cat/",
-	// },
-	// {
-	// 	title: "寅默",
-	// 	url: "https://blog.yinmo19.top/",
-	// 	avatar: "/linkx/yinmo.jpg",
-	// },
-	// {
-	// 	title: "Saurlax",
-	// 	url: "https://saurlax.com/",
-	// 	avatar: "/linkx/saurlax.jpg",
-	// },
-	// {
-	// 	title: "BBB",
-	// 	url: "https://dingbangliu.codeberg.page/",
-	// 	avatar: "/linkx/BBB.jpg",
-	// },
-	// {	
-	// 	title: "Kokoro",
-	// 	url: "https://github.com/Kokoro2336",
-	// 	avatar: "/linkx/Kokoro.jpg",
-	// }
+	{
+		title: "cclvi256",
+		url: "https://cclvi.cc/",
+		avatar: "https://avatars.githubusercontent.com/u/51155451?v=4",
+		description: "A mysterious developer.",
+	},
+	{
+		title: "VaalaCat",
+		avatar: "https://avatars.githubusercontent.com/u/42285362?v=4",
+		url: "https://vaala.cat/",
+		description: "菜猫"
+	},
+	{
+		title: "Saurlax",
+		url: "https://saurlax.com/",
+		avatar: "https://saurlax.com/avatar.jpg",
+		description: "Web, IoT, ML, Cybersecurity, Quantum and Graphics developer"
+	},
+	{
+		title: "寅默",
+		url: "https://blog.yinmo19.top/",
+		avatar: "https://avatars.githubusercontent.com/u/144041694?v=4",
+		description: "Rust, ARM ASM, and Mac."
+	},
+	{
+		title: "BBB",
+		url: "https://dingbangliu.codeberg.page/",
+		avatar: "/linkx/BBB.jpg",
+		description: "Combats avec te défenseurs !"
+	},
+	{	
+		title: "Kokoro",
+		url: "https://github.com/Kokoro2336",
+		avatar: "https://avatars.githubusercontent.com/u/136730884?v=4",
+		description: "可可萝的Github主页，并非博客",
+	}
 ];
 
 export const communities: Friend[] = [
 	{
-		title: "HITSZ 自动化课程攻略共享计划",
-		description: "一份全面的课程攻略，同时包含计算机专业内容。<br>欢迎关注支持！",
+		title: "HITSZ 课程攻略共享计划",
+		description: "一份全面的课程攻略 欢迎关注!",
 		avatar: "https://hoa.moe/images/logo.webp",
 		url: "https://hoa.moe/",
 	},
@@ -67,7 +73,7 @@ export const communities: Friend[] = [
 	{
 		url: "https://osa.moe/",
 		title: "HITSZ OSA",
-		description: "a.k.a Open Source Association<br>HITSZ 开源技术协会",
+		description: "HITSZ 开源技术协会",
 		avatar: "/linkx/OSA.svg",
 	}
 ];
